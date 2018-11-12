0878de7d1e793ebfe955715806aad8d598df3a41
Jair Condori <31250144+NigthDragon5000@users.noreply.github.com>
2018-11-11 19:47:35 -0500

    Adding Optimize , Deploy and Plot
    Have an issue with some variables, do not sort labels well.

--- woess.py
+++ woess.py
@@ -6,73 +6,74 @@ Created on Sat Nov  3 10:13:22 2018
 
 import pandas as pd
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier
 
 class met:
-    def __init__(self,bins=None,breaks=5):
+    def __init__(self,bins=None,nbreaks=10):
         self.bins=bins if bins is None else bins
         self.stat=None 
         self.iv=None
-        self.breaks=breaks
+        self.nbreaks=nbreaks
         self.name=None
+        self.woe=None
+        self.df=None
 
     def fit(self,x,y):
         '''Fitting Information'''
+        if not isinstance(x, pd.Series):
+            x = pd.Series(x.compute())
+        if not isinstance(y, pd.Series):
+            y = pd.Series(y.compute())
         x = pd.Series(x)
         y = pd.Series(y)
         self.name=x.name
         df = pd.DataFrame({"X": x, "Y": y, 'order': np.arange(x.size)})
-        
-#        bins=[]
-#        bins.append(-float('Inf'))
-#        
-#        breaks = self.breaks
-#        br=1/breaks
-#        quant=list(np.arange(br, 1+br, br))
-#        
-#        cuts=df['X'].dropna().quantile(quant)
-#        cuts=list(cuts)
-#        
-#        for i in cuts:
-#            bins.append(i)
-#
-#        bins.append(float('Inf'))
-#        self.bins=bins
-        #cuts,
-        breaks=pd.qcut(df["X"],self.breaks,retbins=True)[1]
-        bins=[]
-        bins.append(-float('Inf'))
-        for i in breaks[1:self.breaks]:
-            bins.append(i)
-        #self.bins=bins
-        bins.append(float('Inf'))
-        self.bins=bins
-        #self.bins = np.append((-float("inf"),), bins[1:-1])
-        #self.bins=np.append(float('Inf'))
-        #cuts, bins = pd.qcut(df["X"],16, retbins=True, labels=False)
+        if self.bins is None:
+           breaks=pd.qcut(df["X"],self.nbreaks,duplicates='drop',retbins=True)[1]
+           breaks=breaks[1:-1]
+           bins=[]
+           bins.append(-float('Inf'))
+           for i in breaks:
+               bins.append(i)
+           bins.append(float('Inf'))
+           self.bins=bins
         q = pd.cut(df['X'], bins=self.bins)
         df['labels']=q.astype(str)
         col_names = {'count_nonzero': 'bad', 'size': 'obs'}
-        self.stat = df.groupby("labels")['Y'].agg([np.mean, np.count_nonzero, np.size]).rename(columns=col_names)   
+        self.stat = df.groupby("labels")['Y'].agg([np.mean, np.count_nonzero, np.size]).rename(columns=col_names).copy()  
         self.stat['bad_perc']=self.stat['bad']/sum(self.stat['bad'])
         self.stat['good']=self.stat['obs']-self.stat['bad']
         self.stat['good_perc']=self.stat['good']/sum(self.stat['good'])
         self.stat['woe'] = np.log(self.stat['good_perc'].values/self.stat['bad_perc'].values)
         self.stat['iv']= (self.stat['good_perc']-self.stat['bad_perc'])*self.stat['woe']
         self.iv=sum(self.stat['iv'])
-       # return quant
+        self.df=df
+                
     
     def deploy(self,df):
         ''' Deploy of bins '''
-        labels = pd.cut(df[self.name],bins=self.bins)
+        labels = pd.cut(df[self.name],bins=self.bins,
+                        labels=self.stat['woe'].tolist())
         return labels  
-
-
-m = met()
-m.fit(dataset['EstimatedSalary'],dataset['Purchased'])
-m.stat
-m.iv
-m.bins
-
-dataset['deploy']=m.deploy(dataset)
+    
+    def plot(self):
+        self.stat['index'] = self.stat.index
+        ''' Plot in function of bad rate'''
+        return self.stat.plot(kind='bar',x='index',y='mean',color='blue')
+    
+    def optimize(self,depth=2,criterion='gini'):
+        clf = DecisionTreeClassifier(criterion='gini',random_state=0,
+                                     max_depth=depth)
+        
+        clf.fit(self.df['X'][:, None],self.df['Y'])
+        breaks=clf.tree_.threshold[clf.tree_.threshold>-2]
+        breaks=sorted(breaks)
+        bins=[]
+        bins.append(-float('Inf'))
+        for i in breaks:
+            bins.append(i)
+        bins.append(float('Inf'))
+        self.bins=bins
+        self.fit(self.df['X'],self.df['Y'])
 
 
