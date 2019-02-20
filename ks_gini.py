2e2d019d8a82ad6132f4f9912d985f97c8ef5ee7
Jair Condori <31250144+NigthDragon5000@users.noreply.github.com>
2019-02-20 16:26:09 -0500

    Update ks_gini.py
    Adding optional plotting

--- ks_gini.py
+++ ks_gini.py
@@ -20,25 +20,25 @@ def ks(test,pred):
 
 
 
-
-
-def gini(test,pred):
+def gini(test,pred,plot=False):
     
-    import matplotlib.pyplot as plt
+    
     logit_roc_auc_train = roc_auc_score(test, pred)
-    fpr, tpr, thresholds = roc_curve(test, pred)
-    plt.figure()
-    plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc_train)
-    plt.plot(fpr, tpr)
-    plt.plot([0, 1], [0, 1],'r--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic TRAIN')
-    plt.legend(loc="lower right")
-    plt.savefig('Log_ROC_train')
-    plt.show()
+#fpr, tpr, thresholds = roc_curve(y_validation, logisticRegr.predict_proba(x_validation)[:,1])
+    if plot:
+        fpr, tpr, thresholds = roc_curve(test, pred)
+        plt.figure()
+        plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc_train)
+        plt.plot(fpr, tpr)
+        plt.plot([0, 1], [0, 1],'r--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic TRAIN')
+        plt.legend(loc="lower right")
+        plt.savefig('Log_ROC_train')
+        plt.show()
+        
     return logit_roc_auc_train,logit_roc_auc_train*2-1
 
-#gini(base['PD_12M'],base['SCORE'])
