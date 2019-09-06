e5513313ff8b59e5c1c3d337d19fb0c3f666b7c7
Jair Condori <31250144+NigthDragon5000@users.noreply.github.com>
2019-09-06 10:59:39 -0500

    Create tseries_processing.py

---
+++ tseries_processing.py
@@ -0,0 +1,150 @@
+"""
+Created on Fri Sep  6 10:37:52 2019
+
+@author: jcondori
+"""
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.stats.diagnostic import acorr_breusch_godfrey
+from statsmodels.graphics.gofplots import qqplot
+from matplotlib import pyplot
+from statsmodels.stats.stattools import durbin_watson
+from scipy.stats import shapiro
+from statsmodels.stats.diagnostic import het_white
+import matplotlib.pyplot as plt
+    
+
+def get_month(x):
+    if x=='Ene':
+        y=1
+    elif x=='Feb':
+        y=2
+    elif x=='Mar':
+        y=3
+    elif x=='Abr':
+        y=4
+    elif x=='May':
+        y=5
+    elif x=='Jun':
+        y=6
+    elif x=='Jul':
+        y=7
+    elif x=='Ago':
+        y=8
+    elif x=='Sep':
+        y=9
+    elif x=='Oct':
+        y=10
+    elif x=='Nov':
+        y=11
+    elif x=='Dic':
+        y=12
+    else:
+        y=0
+    return y
+
+
+
+def monthly_dummie(df):
+    '''
+    Inputs:
+        df : Data Frame, must hace a column with Month variables in format Ene,Feb,Sep...
+    Returns:
+        A Data Frame with months in one hot encode format
+    '''
+    df['month_number']=df.apply(lambda row: get_month(row['month']),axis=1)       
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(df['month_number'])
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)   
+    df=pd.concat([df,pd.DataFrame(onehot_encoded)],axis=1)
+    df.drop('month_number',axis=1)
+    return df
+
+
+
+def DFtest(variable,p_value=0.05):
+    result = adfuller(variable)
+    print('ADF Statistic: {}'.format(result[0]))
+    print('p-value: {}'.format(result[1]))
+    print('Critical Values:')
+    for key, value in result[4].items():
+        print('\t{}: {}'.format(key, value))
+    if result[1] <= p_value:
+        print('Conclusion : Stationary')
+    else:
+        print('Conclusion : Non stationary')
+
+
+
+def error_analisis(result,plot=False):
+    '''
+    Inputs:
+        result: Results from Stats after model.fit()
+        plot: True if we want a plot
+    Returns:
+        Print of an statistics analysis of regression errors which includes: Autocorrelation, \
+        Heterokedasticity, Stationarity and Normality
+    '''
+    #Autocorrleation
+    print('----------Durbin Watson-------------')
+    out = durbin_watson(result.resid)
+    print('Durbin Watson is: '+str(out))
+
+    if plot:
+        qqplot(result.resid, line='s')
+        pyplot.show()
+    
+    print('--------Breusch Autocorr-----------')
+
+    bre=acorr_breusch_godfrey(result,nlags=12)
+
+    print('lm: '+str(bre[0]))
+    print('lmpval: '+str(bre[1]))
+    print('fval: '+str(bre[2]))
+    print('fpval: '+str(bre[3]))
+    
+    if bre[1] < 0.05:
+        print('Evidence for autocorrelation')
+    else:
+        print('Not Evidence for autocorrelation')
+    
+    
+    print('-----White Heteroskedasticity------')
+    
+    white_test = het_white(result.resid,  result.model.exog)
+    
+    labels = ['LM Statistic', 'LM-Test p-value', 'F-Statistic', 'F-Test p-value']
+    print(dict(zip(labels, white_test)))
+    
+    if white_test[1] < 0.05:
+        print('Evidence for heteroskedasticity')
+    else:
+        print('Not Evidence for heteroskedasticity')
+    
+    
+    print('----------ADF Test-----------------')
+    DFtest(result.resid)
+    
+    
+    print('----------Shapiro Normality--------')
+    stat, p = shapiro(result.resid)
+    print('Statistics=%.3f, p=%.3f' % (stat, p))
+    # interpret
+    alpha = 0.05
+    if p > alpha:
+    	print('Sample looks Gaussian (fail to reject H0)')
+    else:
+    	print('Sample does not look Gaussian (reject H0)')
+    
+    if plot:
+        residuals = pd.DataFrame(result.resid)
+        plt.show()
+        residuals.plot(kind='kde')
+        plt.show()
+            
+
